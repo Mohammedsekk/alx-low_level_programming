c53c3dec42bf9f90b4d071cb6eef8707aec0f63e
Mohammed sekkat <sek@Mohammed.com>
2023-04-11 19:05:17 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,20 +12,20 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
+	char *ac;
 	unsigned int i = 0;
 
 	if (size == 0)
 		return (NULL);
 
-	arr = (char *)  malloc(sizeof(char) * size);
+	ac = (char *)  malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (0);
 
 	for (i = 0; i < size; i++)
 	{
-		arr[i] = c;
+		ac[i] = c;
 	}
 	return (ac);
 }
