d3ff81117f4e9e256807ad61c8a2ccd569465f3c
Mohammedsekk <seksimo2001@gmail.com>
2023-05-02 23:50:51 +0300

    second try

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,20 +6,21 @@
  *     * Description: return the required result
  *      *
  *       * Return: return integer value
- *        */
+ *
+ **/
 size_t print_listint(const listint_t *h)
 {
-		const listint_t *dir = NULL;
-			size_t count = 0;
+	const listint_t *dir = NULL;
+	size_t count = 0;
 
-				dir = h;
-					if (dir == NULL)
-								return (0);
-						while (dir != NULL)
-								{
-											printf("%d\n", dir->n);
-													count++;
-															dir = dir->next;
-																}
-							return (count);
+	dir = h;
+	if (dir == NULL)
+		return (0);
+	while (dir != NULL)
+	{
+		printf("%d\n", dir->n);
+		count++;
+		dir = dir->next;
+		return (count);
+	}
 }
